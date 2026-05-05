cee63b4074d15ae13f50821ed69415ca912ffa5d
mrezaaxx <rezamuhammad890@gmail.com>
2026-05-05 22:24:41 +0700

    fix: restore truncated api/index.php

--- api/index.php
+++ api/index.php
@@ -29,4 +29,24 @@ foreach ([
 }
 
 $_ENV['APP_STORAGE_PATH'] = $storagePath;
-$
+$_ENV['APP_BOOTSTRAP_CACHE'] = $bootstrapCache;
+
+// Run migrations once per Lambda instance
+$dbPath = '/tmp/database.sqlite';
+$migrationFlag = '/tmp/hypercare-migrated';
+
+if (! file_exists($dbPath)) {
+    touch($dbPath);
+}
+
+if (! file_exists($migrationFlag)) {
+    $appRoot = dirname(__DIR__);
+    shell_exec("cd {$appRoot} && php artisan migrate --force 2>&1");
+    touch($migrationFlag);
+}
+
+$_SERVER['SCRIPT_FILENAME'] = $publicPath.'/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['DOCUMENT_ROOT'] = $publicPath;
+
+require $publicPath.'/index.php';
